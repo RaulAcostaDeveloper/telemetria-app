d5b0ca8219357ae77032fc678bb1a2151d254639
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-02-28 11:41:00 -0600

    Dejar lista la estructura del proyecto

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,13 +1,12 @@
 import { AuthForm } from "@/modules/auth/components";
 import { LanguageSelector } from "@/modules/global/language/utils/languageSelector";
-import Link from "next/link";
 
 export default function Login() {
   const LANGUAGE = LanguageSelector();
 
   return (
     <div>
-      Login page <Link href={"/register"}>{LANGUAGE?.auth.linkToRegister}</Link>
+      Login page
       <p>{LANGUAGE?.auth.principalTitle}</p>
       <AuthForm />
     </div>
