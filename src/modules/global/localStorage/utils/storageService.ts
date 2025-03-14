9fd5eb6e437afeaee9cf9a62fec94500d10ac4d4
raul.acosta.transtelemetrix <raul.acosta@transtelemetrix.com>
2025-03-14 15:55:35 -0600

    Separar los métodos en funciones

--- src/modules/global/localStorage/utils/storageService.ts
+++ src/modules/global/localStorage/utils/storageService.ts
@@ -5,60 +5,63 @@ interface StorageItem<T> {
 
 // Clase para gestionar el LocalStorage
 
-export const StorageService = {
-  /**
-   * Guarda un valor en localStorage con una clave específica.
-   * Si se proporciona `ttl`, se establece una fecha de expiración.
-   */
-  setItem<T>(key: string, value: T, ttl?: number): void {
-    try {
-      const data: StorageItem<T> = { value };
-      if (ttl) {
-        data.expiry = Date.now() + ttl;
-      }
-      localStorage.setItem(key, JSON.stringify(data));
-    } catch (error) {
-      console.error(`Error guardando ${key} en localStorage`, error);
+/**
+ * Guarda un valor en localStorage con una clave específica.
+ * Si se proporciona `ttl`, se establece una fecha de expiración.
+ */
+export const localStorageSetItem = <T>(
+  key: string,
+  value: T,
+  ttl?: number
+): void => {
+  try {
+    const data: StorageItem<T> = { value };
+    if (ttl) {
+      data.expiry = Date.now() + ttl;
     }
-  },
+    localStorage.setItem(key, JSON.stringify(data));
+  } catch (error) {
+    console.error(`Error guardando ${key} en localStorage`, error);
+  }
+};
 
-  /**
-   * Obtiene un valor de localStorage.
-   * Si tiene expiración y ya ha caducado, lo elimina y devuelve `null`.
-   */
-  getItem<T>(key: string): T | null {
-    try {
-      const item = localStorage.getItem(key);
-      if (!item) return null;
+/**
+ * Obtiene un valor de localStorage.
+ * Si tiene expiración y ya ha caducado, lo elimina y devuelve `null`.
+ */
+export const localStorageGetItem = <T>(key: string): T | null => {
+  try {
+    const item = localStorage.getItem(key);
+    if (!item) return null;
 
-      const data: StorageItem<T> = JSON.parse(item);
+    const data: StorageItem<T> = JSON.parse(item);
 
-      if (data.expiry && Date.now() > data.expiry) {
-        localStorage.removeItem(key);
-        return null;
-      }
-
-      return data.value;
-    } catch (error) {
-      console.error(`Error obteniendo ${key} de localStorage`, error);
+    if (data.expiry && Date.now() > data.expiry) {
+      localStorage.removeItem(key);
       return null;
     }
-  },
 
-  /**
-   * Elimina un ítem específico de localStorage.
-   */
-  removeItem(key: string): void {
-    localStorage.removeItem(key);
-  },
-
-  /**
-   * Limpia todo el localStorage.
-   */
-  clearAllStorage(): void {
-    localStorage.clear();
-  },
+    return data.value;
+  } catch (error) {
+    console.error(`Error obteniendo ${key} de localStorage`, error);
+    return null;
+  }
 };
+
+/**
+ * Elimina un ítem específico de localStorage.
+ */
+export const localStorageRemoveItem = (key: string): void => {
+  localStorage.removeItem(key);
+};
+
+/**
+ * Cuidado: Limpia todo el localStorage.
+ */
+export const localStorageClearAllStorage = (): void => {
+  localStorage.clear();
+};
+
 // Ejemplo:
 // const initialCalendarDate = { // obtenerlo del calendario
 //  initialDate: "28-3-2025, 14:00:00",
@@ -66,24 +69,24 @@ export const StorageService = {
 // }
 //
 // Con expiración
-// StorageService.setItem(STORAGE_KEYS.CALENDAR, initialCalendarDate, 3600000);
+// localStorageSetItem(STORAGE_KEYS.CALENDAR, initialCalendarDate, 3600000);
 //
 // Sin expiración
-// StorageService.setItem(STORAGE_KEYS.CALENDAR, initialCalendarDate);
+// localStorageSetItem(STORAGE_KEYS.CALENDAR, initialCalendarDate);
 //
 // Recuperar datos
 // Primer render
-// const calendarRangeStorage: CalendarInterface | null = StorageService.getItem(STORAGE_KEYS.CALENDAR);
+// const calendarRangeStorage: CalendarInterface | null = localStorageGetItem(STORAGE_KEYS.CALENDAR);
 // if (calendarRangeStorage !== null) {
 //    // valor del estado local o global
 //    setCalendarRange(calendarRangeStorage); // Del storage
 //  } else {
 //    setCalendarRange(initialCalendarDate); // Del valor predeterminado o inicial
-//    StorageService.setItem(STORAGE_KEYS.CALENDAR, initialCalendarDate);
+//    localStorageSetItem(STORAGE_KEYS.CALENDAR, initialCalendarDate);
 //  }
 //
 // Actualizar el LocalStorage
 // Al cambiar el valor
 // if (calendarRange !== null) {
-//      StorageService.setItem(STORAGE_KEYS.CALENDAR, calendarRange);
+//      localStorageSetItem(STORAGE_KEYS.CALENDAR, calendarRange);
 // }
