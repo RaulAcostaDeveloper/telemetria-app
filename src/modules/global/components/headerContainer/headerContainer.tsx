1a4dbd9279aaa9b34efc4264d9051746e44ceb3b
raul.acosta.transtelemetrix <raul.acosta@transtelemetrix.com>
2025-03-14 13:46:59 -0600

    Usar Flex en el contenedor del Header y Menu

--- src/modules/global/components/headerContainer/headerContainer.tsx
+++ src/modules/global/components/headerContainer/headerContainer.tsx
@@ -1,9 +1,10 @@
 import { Header } from "../header/header";
 import { Menu } from "../menu/menu";
+import styles from "./headerContainer.module.css";
 
 export const HeaderContainer = () => {
   return (
-    <div>
+    <div className={`${styles.headerContainer}`}>
       <Menu />
       <Header />
     </div>
