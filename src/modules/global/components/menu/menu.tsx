14b152829b07ca6de290ac2c081afa0cf4fe2ba4
raul.acosta.transtelemetrix <raul.acosta@transtelemetrix.com>
2025-03-14 14:00:52 -0600

    Una manera de manejar el LocalStorage

--- src/modules/global/components/menu/menu.tsx
+++ src/modules/global/components/menu/menu.tsx
@@ -1,9 +1,32 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./menu.module.css";
+import { StorageService } from "../../localStorage/utils/storageService";
+import { STORAGE_KEYS } from "../../localStorage/constants/storageKeys";
 
 export const Menu = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  // Es importante que el valor inicial sea null
+  const [isOpen, setIsOpen] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    // Inicialización del valor
+    const defaultValue: boolean = true;
+    const storedValue: boolean | null = StorageService.getItem(
+      STORAGE_KEYS.MENU_OPEN
+    );
+    // Si existe el valor en LocalStorage, lo asigna.
+    // Si no existe, asigna el valor por defecto.
+    setIsOpen(storedValue !== null ? storedValue : defaultValue);
+  }, []);
+
+  useEffect(() => {
+    // Actualización del elemento en LocalStorage
+    // Usa el valor null para evita actualizarlo en el primer render
+    if (isOpen !== null) {
+      StorageService.setItem(STORAGE_KEYS.MENU_OPEN, isOpen);
+    }
+  }, [isOpen]);
+
   return (
     <div
       className={`${styles.menu} ${
---
+++ src/modules/global/localStorage/constants/storageKeys.ts
@@ -0,0 +1,5 @@
+// Dar de alta nuevos elementos de LocalStorage
+export const STORAGE_KEYS = {
+  CALENDAR: "calendar_date_range",
+  MENU_OPEN: "is_menu_open",
+};
