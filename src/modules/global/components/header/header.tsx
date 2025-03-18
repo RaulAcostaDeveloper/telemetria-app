4ce1733e2344144137dea501cd9e0a2fc433caaa
edgar-chavez-23 <edgar.chavez@transtelemetrix.com>
2025-03-18 11:14:30 -0600

    add mui icons needed libraries and fix style issues

--- src/modules/global/components/header/header.tsx
+++ src/modules/global/components/header/header.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import styles from "./header.module.css";
 
 export const Header = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const goBack = () => {
+    if (pathname !== "/") {
+      router.back();
+    }
+  };
+
   return (
     <header className={`${styles.header}`}>
-      <nav className={styles.navBar}></nav>
+      <nav className={styles.navBar}>
+        <button onClick={goBack} className={styles.returnButton}>
+          <ArrowBackIcon />
+        </button>
+      </nav>
     </header>
   );
 };
