168e7405227fb58a7951de5fc05231dfadd7b3c8
raul.acosta.transtelemetrix <raul.acosta@transtelemetrix.com>
2025-03-31 15:26:40 -0600

    Refactor y solución de algunos bugs visuales

--- src/app/(pages)/fuel/page.tsx
+++ src/app/(pages)/fuel/page.tsx
@@ -4,15 +4,6 @@ import { Table } from "@/modules/global/components";
 export default function Fuel() {
   return (
     <div>
-      <Table
-        title="Table"
-        showCreateButton
-        showDelete
-        showView
-        showEdit
-        data={[{}]}
-        columns={[]}
-      />
       <FuelTabs />
     </div>
   );
