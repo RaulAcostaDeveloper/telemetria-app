e06f4213bca9b928bcb7fa4c1845c7a162ded8ff
raul.acosta.transtelemetrix <raul.acosta@transtelemetrix.com>
2025-04-14 14:17:37 -0600

    Mostrar el avance en la página de fuel

--- src/app/(pages)/fuel/page.tsx
+++ src/app/(pages)/fuel/page.tsx
@@ -1,8 +1,18 @@
 import { FuelTabs } from "@/modules/fuel/components";
+import { Table } from "@/modules/global/components";
 
 export default function Fuel() {
   return (
     <div>
+      <Table
+        title="Mi tabla"
+        columns={[]}
+        data={[]}
+        showCreateButton
+        showDelete
+        showEdit
+        showView
+      />
       <FuelTabs />
     </div>
   );
--- src/modules/fuel/components/fuelTabs/fuelTabs.tsx
+++ src/modules/fuel/components/fuelTabs/fuelTabs.tsx
@@ -1,5 +1,6 @@
 import { LanguageSelector } from "@/modules/global/language/utils/languageSelector";
-import { TabsContent } from "@/modules/global/components";
+import { Table, TabsContent } from "@/modules/global/components";
+import styles from "./fuelTabs.module.css";
 
 export const FuelTabs: React.FC = () => {
   const LANGUAGE = LanguageSelector();
@@ -12,7 +13,28 @@ export const FuelTabs: React.FC = () => {
     <TabsContent
       tabOptions={tabOptions}
       tabContents={[
-        <div key={1}>1</div>,
+        <div key={1}>
+          <div className={styles.tablesContainer}>
+            <Table
+              title="Tabla dentro de un tab"
+              columns={[]}
+              data={[]}
+              showCreateButton
+              showDelete
+              showEdit
+              showView
+            />
+            <Table
+              title="Tabla a un lado"
+              columns={[]}
+              data={[]}
+              showCreateButton
+              showDelete
+              showEdit
+              showView
+            />
+          </div>
+        </div>,
         <div key={2}>2</div>,
         <div key={3}>3</div>,
       ]}
