6730e0aaea608b87a84ab38ad6cfa070043ff772
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-04-16 15:14:44 -0600

    Menú fijo

--- src/app/(pages)/fuel/page.tsx
+++ src/app/(pages)/fuel/page.tsx
@@ -1,8 +1,36 @@
 import { FuelTabs } from "@/modules/fuel/components";
+import { Table } from "@/modules/global/components";
 
 export default function Fuel() {
   return (
     <div>
+      <Table
+        title="Mi tabla"
+        showCreateButton
+        showDelete
+        showEdit
+        showView
+        data={[]}
+        columns={[]}
+      />
+      <Table
+        title="Mi tabla"
+        showCreateButton
+        showDelete
+        showEdit
+        showView
+        data={[]}
+        columns={[]}
+      />
+      <Table
+        title="Mi tabla"
+        showCreateButton
+        showDelete
+        showEdit
+        showView
+        data={[]}
+        columns={[]}
+      />
       <FuelTabs />
     </div>
   );
