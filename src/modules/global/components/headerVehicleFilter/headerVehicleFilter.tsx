e24adf6ab67d6bf7d15226fb0c949c630db4f6af
edgar-chavez-23 <edgar.chavez@transtelemetrix.com>
2025-05-20 15:33:41 -0600

    fix imports order

--- src/modules/global/components/headerVehicleFilter/headerVehicleFilter.tsx
+++ src/modules/global/components/headerVehicleFilter/headerVehicleFilter.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
