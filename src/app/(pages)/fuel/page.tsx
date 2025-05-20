29c101a36b4ec12eec75f4d8096e5ea7998ddb83
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-05-20 11:59:52 -0600

    Ordenamiento ascendente y descendente

--- src/app/(pages)/fuel/page.tsx
+++ src/app/(pages)/fuel/page.tsx
@@ -1,7 +1,109 @@
+"use client";
+import { Table } from "@/modules/global/components";
+import {
+  columnsTable,
+  dataTable,
+} from "@/modules/global/components/table/table.model";
+import { useLanguage } from "@/modules/global/language/components/languageProvider/languageProvider";
+
+const tableColumns: columnsTable = [
+  {
+    columnName: "Zona",
+    defaultSpace: 3,
+    orderColumn: true,
+  },
+  {
+    columnName: "Perfil",
+    defaultSpace: 3,
+    filterSelector: true,
+    orderColumn: true,
+  },
+  {
+    columnName: "País",
+    defaultSpace: 3,
+    filterSelector: true,
+    orderColumn: true,
+  },
+  {
+    columnName: "Id",
+    defaultSpace: 1,
+  },
+];
+
+const tableData: dataTable = [
+  {
+    zone: "Zone A12",
+    profile: "Geo standard",
+    country: "México",
+    idVehicle: "101",
+  },
+  {
+    zone: "Zone B07",
+    profile: "Eco drive",
+    country: "Colombia",
+    idVehicle: "102",
+  },
+  {
+    zone: "Zone C19",
+    profile: "Geo gravity",
+    country: "Chile",
+    idVehicle: "103",
+  },
+  {
+    zone: "Zone D03",
+    profile: "Urban ride",
+    country: "Argentina",
+    idVehicle: "104",
+  },
+  {
+    zone: "Zone E22",
+    profile: "Highway master",
+    country: "Perú",
+    idVehicle: "105",
+  },
+  {
+    zone: "Zone F11",
+    profile: "Eco drive",
+    country: "Brasil",
+    idVehicle: "106",
+  },
+  {
+    zone: "Zone G05",
+    profile: "Geo standard",
+    country: "México",
+    idVehicle: "107",
+  },
+  {
+    zone: "Zone H14",
+    profile: "Urban ride",
+    country: "Ecuador",
+    idVehicle: "108",
+  },
+  {
+    zone: "Zone I09",
+    profile: "Highway master",
+    country: "Uruguay",
+    idVehicle: "109",
+  },
+  {
+    zone: "Zone J01",
+    profile: "Geo gravity",
+    country: "Paraguay",
+    idVehicle: "110",
+  },
+];
+
 export default function Fuel() {
+  const LANGUAGE = useLanguage();
   return (
     <div>
-      <h1>Fuel page</h1>
+      <h1>Fuel page</h1>{" "}
+      <Table
+        LANGUAGE={LANGUAGE}
+        title="Table example"
+        columns={tableColumns}
+        data={tableData}
+      />
     </div>
   );
 }
