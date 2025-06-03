df08f11583c47fb1d46e17fa41ac057e5114209c
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-06-03 10:42:18 -0600

    Data mock para la pantalla de gestión

---
+++ src/modules/global/dataMock/groups/groups.ts
@@ -0,0 +1,20 @@
+export const groupsDataMock = {
+  code: 200,
+  message: "Ok",
+  value: {
+    groups: [
+      {
+        id: 81640,
+        name: "Paro",
+      },
+      {
+        id: 81656,
+        name: "Grupo Demo",
+      },
+      {
+        id: 87855,
+        name: "Prueba Ivan",
+      },
+    ],
+  },
+};
