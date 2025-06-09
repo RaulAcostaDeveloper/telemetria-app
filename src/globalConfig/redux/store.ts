24b476248cf7bb94cd9fbd7fbdd28eb94633754c
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-06-09 16:57:04 -0600

    Traer vehículos en el inicio de sesión

--- src/globalConfig/redux/store.ts
+++ src/globalConfig/redux/store.ts
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "@/globalConfig/redux/slices/authSlice";
 import calendarReducer from "@/globalConfig/redux/slices/calendarSlice";
 import fuelSummaryReducer from "@/globalConfig/redux/slices/fuelSummarySlice";
 import languageReducer from "@/globalConfig/redux/slices/languageSlice";
-import authReducer from "@/globalConfig/redux/slices/authSlice";
+import vehiclesSlice from "@/globalConfig/redux/slices/vehiclesSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     calendar: calendarReducer,
     fuelSummary: fuelSummaryReducer,
     languageOption: languageReducer,
-    auth: authReducer,
+    vehicles: vehiclesSlice,
   },
   devTools: true,
 });
