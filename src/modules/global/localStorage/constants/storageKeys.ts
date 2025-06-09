3b9ca535a511518db99904b02a4096492cefdd41
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-06-09 14:54:32 -0600

    LocalStorage en formulario de filtrado

--- src/modules/global/localStorage/constants/storageKeys.ts
+++ src/modules/global/localStorage/constants/storageKeys.ts
@@ -2,4 +2,8 @@
 export const STORAGE_KEYS = {
   CALENDAR: "calendar_date_range",
   MENU_OPEN: "is_menu_open",
+  FUEL_FILTER_ACCOUNT: "fuel_filter_account",
+  FUEL_FILTER_GROUP: "fuel_filter_group",
+  FUEL_FILTER_BRAND: "fuel_filter_brand",
+  FUEL_FILTER_MODEL: "fuel_filter_model",
 };
