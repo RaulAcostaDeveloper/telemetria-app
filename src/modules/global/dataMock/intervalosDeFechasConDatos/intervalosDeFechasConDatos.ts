2b6c33dff150091e0e37added089edf5bac5350c
Raúl Acosta <raulacostadeveloper@outlook.com>
2025-07-09 13:11:36 -0600

    Archivo de intervalos de fechas que si devuelven datos

---
+++ src/modules/global/dataMock/intervalosDeFechasConDatos/intervalosDeFechasConDatos.ts
@@ -0,0 +1,102 @@
+export const intervalosDeFechasConDatos = [
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:01",
+    },
+    endDate: {
+      $date: "2024-09-06T15:48:01",
+    },
+    count: 3889,
+    DeviceId: "862599050434198",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:02",
+    },
+    endDate: {
+      $date: "2024-09-06T15:55:52",
+    },
+    count: 8199,
+    DeviceId: "863457050462926",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:02",
+    },
+    endDate: {
+      $date: "2024-09-06T15:56:01",
+    },
+    count: 8012,
+    DeviceId: "863457050464039",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:02",
+    },
+    endDate: {
+      $date: "2024-09-06T15:56:01",
+    },
+    count: 8012,
+    DeviceId: "863457050464039",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:02",
+    },
+    endDate: {
+      $date: "2024-09-02T11:42:02",
+    },
+    count: 1783,
+    DeviceId: "863457050464062",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:02",
+    },
+    endDate: {
+      $date: "2024-09-06T15:55:15",
+    },
+    count: 7963,
+    DeviceId: "863457050462405",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:03",
+    },
+    endDate: {
+      $date: "2024-09-06T15:55:44",
+    },
+    count: 8269,
+    DeviceId: "863457050462058",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:04",
+    },
+    endDate: {
+      $date: "2024-09-03T01:17:28",
+    },
+    count: 2610,
+    DeviceId: "863457050463387",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:05",
+    },
+    endDate: {
+      $date: "2024-09-04T18:55:59",
+    },
+    count: 5094,
+    DeviceId: "863457050461514",
+  },
+  {
+    startDate: {
+      $date: "2024-09-01T06:00:06",
+    },
+    endDate: {
+      $date: "2024-09-06T15:56:25",
+    },
+    count: 8289,
+    DeviceId: "863457050463494",
+  },
+];
