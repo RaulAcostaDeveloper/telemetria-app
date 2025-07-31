cca1d854a198363dbb97fe90feb48e6eb4fc6428
Marlon Vargas <marlon.vargas@transtelemetrix.com>
2025-07-31 17:32:36 -0600

    Llamada a dispatch en componente de header

--- src/modules/global/components/header/headerTextContent/headerTextWords.tsx
+++ src/modules/global/components/header/headerTextContent/headerTextWords.tsx
@@ -1,20 +1,36 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuth } from "@/modules/auth/utils";
+import { fetchVehicleByImei } from "@/globalConfig/redux/slices/vehicleByImeiSlice";
+
 import styles from "./headerTextWords.module.css";
 
 // Tipado
-import { RootState } from "@/globalConfig/redux/store";
+import { AppDispatch, RootState } from "@/globalConfig/redux/store";
 import { LanguageInterface } from "@/modules/global/language/constants/language.model";
 
 interface Props {
   LANGUAGE: LanguageInterface;
   section: string;
+  url: string;
 }
 
-export const HeaderTextWords = ({ LANGUAGE, section }: Props) => {
+export const HeaderTextWords = ({ LANGUAGE, section, url }: Props) => {
+  const { isAuthenticated } = useAuth();
+  const dispatch = useDispatch<AppDispatch>();
+  const segmentsURL = url.split("/").filter(Boolean);
+  const lastSegmentURL = segmentsURL[segmentsURL.length - 1];
+
   const { vehicleByImeiData, vehicleByImeiStatus } = useSelector(
     (state: RootState) => state.vehicleByImei
   );
+
+  useEffect(() => {
+    if (isAuthenticated && lastSegmentURL && lastSegmentURL.length > 3) {
+      dispatch(fetchVehicleByImei({ imei: lastSegmentURL }));
+    }
+  }, [dispatch, isAuthenticated, lastSegmentURL]);
 
   function jsxOption(): JSX.Element {
     if ("succeeded" === vehicleByImeiStatus && vehicleByImeiData) {
