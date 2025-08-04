749368bc32e9e8b01e63dd6f650e5ae39c71286a
Marlon Vargas <marlon.vargas@transtelemetrix.com>
2025-08-04 16:26:03 -0600

    Homologado telemetryOBDHome, y de paso el bug de mostrar grafica barras

--- src/app/(pages)/(restricted)/home/page.tsx
+++ src/app/(pages)/(restricted)/home/page.tsx
@@ -1,16 +1,17 @@
 "use client";
-import {
-  HomeBriefFuelDataProvider,
-  HomeBriefTelemetryOBD,
-} from "@/modules/home/components";
+import { HomeBriefFuelDataProvider } from "@/modules/home/components";
+import { useLanguage } from "@/modules/global/language/components/languageProvider/languageProvider";
+import { TelemetryHome } from "@/modules/telemetryObd/components";
 
 export default function Home() {
+  const LANGUAGE = useLanguage();
+
   return (
     <div>
       {/* <section> */}
       <HomeBriefFuelDataProvider />
       {/* </section> */}
-      <HomeBriefTelemetryOBD />
+      <TelemetryHome LANGUAGE={LANGUAGE} />
     </div>
   );
 }
--- src/modules/home/components/index.ts
+++ src/modules/home/components/index.ts
@@ -1,2 +1,1 @@
 export * from "./homeBriefFuelDataProvider/homeBriefFuelDataProvider";
-export * from "./homeBriefTelemetryOBD/homeBriefTelemetryOBD";
