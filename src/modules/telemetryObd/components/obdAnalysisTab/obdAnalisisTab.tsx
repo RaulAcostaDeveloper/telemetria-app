cf98310d2bfb5d975a32b873fb80d7a543e54a6c
raul.acosta.transtelemetrix <raulacostadeveloper@outlook.com>
2025-10-03 17:08:41 -0600

    Velocidad máxima en reporte de obd

--- src/modules/telemetryObd/components/obdAnalysisTab/obdAnalisisTab.tsx
+++ src/modules/telemetryObd/components/obdAnalysisTab/obdAnalisisTab.tsx
@@ -1,7 +1,6 @@
 import styles from "./obdAnalisisTab.module.css";
 import { LanguageInterface } from "@/global/language/constants/language.model";
 import { MetricItem } from "@/modules/fuel/components/fuelPerformanceMetrics/metricItem/metricItem";
-import { NO_DATA } from "@/global/utils/ndIfEmpty";
 import { ObdTravelMetricsDataValues } from "@/global/redux/serviceSlices/obdTravelMetricsSlice";
 import { VehicleByImei } from "@/global/redux/serviceSlices/vehicleByImeiSlice";
 
@@ -104,7 +103,7 @@ export const ObdAnalysisTab = ({
           LANGUAGE={LANGUAGE}
           metric="km/h"
           name={LANGUAGE.onBoardDiagnosticsVehicle.analysisTab.allowedMaxSpeed}
-          value={NO_DATA} // pendiente
+          value={obdAnalyticsData.maxSpeedAllowed}
           isLast
         />
       </div>
