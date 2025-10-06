94504e627969cb4e1efa0286e59a8d0e6e707cf3
raul.acosta.transtelemetrix <raul.acosta@transtelemetrix.com>
2025-10-06 14:48:12 -0600

    Archivo desactualizado

--- src/modules/fuel/components/fuelFilter/fuelFilter.tsx
+++ src/modules/fuel/components/fuelFilter/fuelFilter.tsx
@@ -1,31 +1,34 @@
-"use client";
-import { useState } from "react";
+// "use client";
+// import { useState } from "react";
 
-import styles from "./fuelFilter.module.css";
-import { FuelFilterContent } from "./fuelFilterContent/fuelFilterContent";
-import { FuelFilterHeader } from "./fuelFilterHeader/fuelFilterHeader";
-import { LanguageInterface } from "@/global/language/constants/language.model";
+// import styles from "./fuelFilter.module.css";
+// // import { FuelFilterContent } from "./fuelFilterContent/fuelFilterContent";
+// import { FuelFilterHeader } from "./fuelFilterHeader/fuelFilterHeader";
+// import { LanguageInterface } from "@/global/language/constants/language.model";
 
-interface Props {
-  LANGUAGE: LanguageInterface;
-  callFetchFuelSummary: () => void;
-}
+// interface Props {
+//   LANGUAGE: LanguageInterface;
+//   callFetchFuelSummary: () => void;
+// }
 
-export const FuelFilter = ({ LANGUAGE, callFetchFuelSummary }: Props) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  return (
-    <div className={styles.fuelFilter}>
-      <FuelFilterHeader
-        LANGUAGE={LANGUAGE}
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      />
-      {isOpen && (
-        <FuelFilterContent
-          LANGUAGE={LANGUAGE}
-          callFetchFuelSummary={callFetchFuelSummary}
-        />
-      )}
-    </div>
-  );
-};
+// export const FuelFilter = ({
+//   LANGUAGE,
+// }: // callFetchFuelSummary
+// Props) => {
+//   const [isOpen, setIsOpen] = useState<boolean>(false);
+//   return (
+//     <div className={styles.fuelFilter}>
+//       <FuelFilterHeader
+//         LANGUAGE={LANGUAGE}
+//         isOpen={isOpen}
+//         setIsOpen={setIsOpen}
+//       />
+//       {/* {isOpen && (
+//         <FuelFilterContent
+//           LANGUAGE={LANGUAGE}
+//           callFetchFuelSummary={callFetchFuelSummary}
+//         />
+//       )} */}
+//     </div>
+//   );
+// };
