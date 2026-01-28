68e7b59b8e899bd2be33e01f784d82d77fee95cf
Marlon Vargas <marlon.vargas@transtelemetrix.com>
2026-01-28 13:12:16 -0600

    Al validar si no es primer render, evito notificacion

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const disableLint = process.env.DISABLE_ESLINT === "true";
 const nextConfig = {
-
-    eslint: {
+  eslint: {
     ignoreDuringBuilds: disableLint,
   },
   // opcional: para ignorar errores de TypeScript también
   typescript: {
     ignoreBuildErrors: disableLint,
   },
+
+  // Para deshabilitar strict mode de modo desarrollo en pruebas de efectos con refs, mapas, gráficos.
+  // Regresar a que quede comentado siempre. Uso manual.
+  // reactStrictMode: false,
 };
 
 export default nextConfig;
