22b0c826d0f41f534f8dd49af6b68535418c9e55
raul.acosta.transtelemetrix <raulacostadeveloper@outlook.com>
2026-03-13 21:59:41 -0600

    Fit para deploy

--- src/modules/zones/components/zoneSingleInfo/zoneSingleInfo.tsx
+++ src/modules/zones/components/zoneSingleInfo/zoneSingleInfo.tsx
@@ -1,4 +1,4 @@
-import { toLocalDateTime } from "@/global/utils/dateUtils";
+// import { toLocalDateTime } from "@/global/utils/dateUtils";
 import styles from "./zoneSingleInfo.module.css";
 import { zoneDataMock } from "@/global/dataMock/z0n3sD4t4M0ck";
 
@@ -14,11 +14,11 @@ export const ZoneSingleInfo = ({ allZoneData }: Props) => {
       <div className={["frame", styles.frame].join(" ")}>
         <div className={styles.row}>
           <div>Fecha de carga no conciliada</div>
-          <div>{toLocalDateTime(allZoneData.fechaCargaNoConciliada ?? "")}</div>
+          {/* <div>{toLocalDateTime(allZoneData.fechaCargaNoConciliada ?? "")}</div> */}
         </div>
         <div className={styles.row}>
           <div>Dirección</div>
-          <div>{allZoneData.address}</div>
+          {/* <div>{allZoneData.address}</div> */}
         </div>
         <div className={styles.row}>
           <div>Latitud</div>
@@ -30,47 +30,47 @@ export const ZoneSingleInfo = ({ allZoneData }: Props) => {
         </div>
         <div className={styles.row}>
           <div>Odómetro</div>
-          <div>{`${allZoneData.odometer} Km`}</div>
+          {/* <div>{`${allZoneData.odometer} Km`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Velocidad</div>
-          <div>{`${allZoneData.speed} Km/h`}</div>
+          {/* <div>{`${allZoneData.speed} Km/h`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Ignición</div>
-          <div>{allZoneData.ignition ? "Encendido" : "Apagado"}</div>
+          {/* <div>{allZoneData.ignition ? "Encendido" : "Apagado"}</div> */}
         </div>
         <div className={styles.row}>
           <div>Batería del dispositivo</div>
-          <div>{`${allZoneData.battery}%`}</div>
+          {/* <div>{`${allZoneData.battery}%`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Energía principal</div>
-          <div>{`${allZoneData.mainEnergy} V`}</div>
+          {/* <div>{`${allZoneData.mainEnergy} V`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Origen</div>
-          <div>{allZoneData.origin}</div>
+          {/* <div>{allZoneData.origin}</div> */}
         </div>
         <div className={styles.row}>
           <div>Magnitud</div>
-          <div>{`${allZoneData.magnitude} L`}</div>
+          {/* <div>{`${allZoneData.magnitude} L`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Combustible inicial</div>
-          <div>{`${allZoneData.startFuel} L`}</div>
+          {/* <div>{`${allZoneData.startFuel} L`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Combustible final</div>
-          <div>{`${allZoneData.endFuel} L`}</div>
+          {/* <div>{`${allZoneData.endFuel} L`}</div> */}
         </div>
         <div className={styles.row}>
           <div>Inició</div>
-          <div>{toLocalDateTime(allZoneData.initialDate ?? "")}</div>
+          {/* <div>{toLocalDateTime(allZoneData.initialDate ?? "")}</div> */}
         </div>
         <div className={styles.row}>
           <div>Finalizó</div>
-          <div>{toLocalDateTime(allZoneData.endDate ?? "")}</div>
+          {/* <div>{toLocalDateTime(allZoneData.endDate ?? "")}</div> */}
         </div>
       </div>
     </div>
